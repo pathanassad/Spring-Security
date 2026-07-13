681f9b51e0e08d8764c00ccd7ca234345234309e
Assad <pathanassad@gmail.com>
2026-07-14 00:35:57 +0530

    verify user from the database

--- src/main/java/com/asad/SpringSecurity/config/SecurityConfig.java
+++ src/main/java/com/asad/SpringSecurity/config/SecurityConfig.java
@@ -1,17 +1,29 @@
 package com.asad.SpringSecurity.config;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
 
      @Bean
      public SecurityFilterChain getSecurityFilterChain(HttpSecurity http) throws Exception{
@@ -23,6 +35,34 @@ public class SecurityConfig {
          return http.build();
 
          // you can also use builder pattern here
-         
+
      }
+
+     @Bean
+     public UserDetailsService userDetailsService(){
+         UserDetails user1 = User
+                 .withDefaultPasswordEncoder()
+                 .username("Altamash")
+                 .password("Pass@123")
+                 .roles("USER")
+                 .build();
+
+         UserDetails user2 = User
+                 .withDefaultPasswordEncoder()
+                 .username("Abdul Karim")
+                 .password("Pass@123")
+                 .roles("Admin")
+                 .build();
+
+         return new InMemoryUserDetailsManager(user1, user2);
+     }
+
+     @Bean
+    public AuthenticationProvider authenticationProvider(){
+         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+         provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+         provider.setUserDetailsService(userDetailsService);
+         return provider;
+     }
+
 }
---
+++ src/main/java/com/asad/SpringSecurity/service/MyUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.asad.SpringSecurity.service;
+
+import com.asad.SpringSecurity.model.UserPrinciple;
+import com.asad.SpringSecurity.model.Users;
+import com.asad.SpringSecurity.repository.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepo repo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Users user = repo.findByUsername(username);
+        if(user == null){
+            System.out.println("User not found");
+            throw new UsernameNotFoundException("user not found");
+        }
+
+
+        return new UserPrinciple(user);
+
+    }
+
+
+}
